80820e995c654c05d40083db8777b56756cae134
15077693d <15077693d@gmail.com>
2020-10-13 00:16:46 +0800

    debug

--- model/dataProcessor.py
+++ model/dataProcessor.py
@@ -8,12 +8,12 @@ class DataProcessor:
         images_folder_path = os.path.join(pathlib.Path(os.path.dirname(__file__)).parent,"images")
         date_folder_path = os.path.join(images_folder_path, date)
         self.json_path = os.path.join(date_folder_path, time)
-        with open(self.json_path, "w") as f:
-            json.dump({},f)
         if not os.path.exists(images_folder_path):
             os.mkdir(images_folder_path)
         if not os.path.exists(date_folder_path):
             os.mkdir(date_folder_path)
+        with open(self.json_path, "w") as f:
+            json.dump({},f)
 
     def output(self, keyword, image_urls):
         with open(self.json_path, "r") as f:
